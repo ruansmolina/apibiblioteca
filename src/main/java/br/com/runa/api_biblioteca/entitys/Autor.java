a564232fc19f5dc0a091131d738b37fd020ed920
Ruan Molina <ruan.molina@hotmail.com>
2025-04-23 23:18:00 -0300

    Correction in Autor and Categoria

--- src/main/java/br/com/runa/api_biblioteca/entitys/Autor.java
+++ src/main/java/br/com/runa/api_biblioteca/entitys/Autor.java
@@ -2,14 +2,20 @@ package br.com.runa.api_biblioteca.entitys;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
 @Data
+@NoArgsConstructor
 public class Autor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String sobrenome;
+    public Autor(Integer id){
+        this.id = id;
+    }
+
 }
--- src/main/java/br/com/runa/api_biblioteca/entitys/Categoria.java
+++ src/main/java/br/com/runa/api_biblioteca/entitys/Categoria.java
@@ -2,13 +2,19 @@ package br.com.runa.api_biblioteca.entitys;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
 @Data
+@NoArgsConstructor
 public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+
+    public Categoria(Integer id){
+        this.id = id;
+    }
 }
